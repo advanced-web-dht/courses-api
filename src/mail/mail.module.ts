6b5545923c4fc768dc9094e729cd8ac29158c0e7
Huy <l.honghuy09@gmail.com>
2021-11-23 00:20:30 +0700

    Fix refresh token

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -6,33 +6,7 @@ import { MailService } from './mail.service';
 import { join } from 'path';
 
 @Module({
-	imports: [
-		ConfigModule.forRoot(),
-		MailerModule.forRoot({
-			transport: {
-				service: 'Gmail',
-				secure: false,
-				auth: {
-					type: 'OAuth2',
-					user: process.env.GG_EMAIL,
-					clientId: process.env.GG_CLIENT_ID,
-					clientSecret: process.env.GG_CLIENT_SECRET,
-					refreshToken: process.env.GG_REFRESH_TOKEN,
-					accessToken: process.env.GG_ACCESS_TOKEN
-				}
-			},
-			defaults: {
-				from: '"No Reply" <noreply@example.com>'
-			},
-			template: {
-				dir: join(__dirname, 'templates'),
-				adapter: new HandlebarsAdapter(),
-				options: {
-					strict: true
-				}
-			}
-		})
-	],
+	imports: [ConfigModule.forRoot()],
 	providers: [MailService],
 	exports: [MailService]
 })
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,19 +1,55 @@
-import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { Interval } from '@nestjs/schedule';
+import { google } from 'googleapis';
+import { createTransport } from 'nodemailer';
 
 @Injectable()
 export class MailService {
-	constructor(private mailerService: MailerService) {}
+	private transport;
+	constructor() {
+		(async () => {
+			await this.updateTransport();
+		})();
+	}
+
+	@Interval(39000)
+	async updateTransport(): Promise<void> {
+		const oAuth2Client = new google.auth.OAuth2(
+			process.env.GG_CLIENT_ID,
+			process.env.GG_CLIENT_SECRET,
+			process.env.GG_REDIRECT
+		);
+		oAuth2Client.setCredentials({ refresh_token: process.env.GG_REFRESH_TOKEN });
+		const accessToken = await oAuth2Client.getAccessToken();
+		this.transport = createTransport(
+			{
+				service: 'gmail',
+				secure: false,
+				auth: {
+					type: 'OAuth2',
+					user: process.env.GG_EMAIL,
+					clientId: process.env.GG_CLIENT_ID,
+					clientSecret: process.env.GG_CLIENT_SECRET,
+					accessToken: accessToken.token
+				}
+			},
+			{
+				from: '"No Reply" <noreply@example.com>'
+			}
+		);
+	}
 
 	async sendInvitationMail(email: string, url: string) {
-		await this.mailerService.sendMail({
+		await this.transport.sendMail({
 			to: email,
 			from: '"No Reply" <noreply@example.com>',
 			subject: 'Welcome to Nice App! Confirm your Email',
-			template: './information',
-			context: {
-				url
-			}
+			html: `<p>Hello,</p>
+				<p>Please click here to join class</p>
+				<p>
+				<a href=${url}>Confirm</a> 
+				</p>
+				<p>If you did not request this email you can safely ignore it.`
 		});
 	}
 }
